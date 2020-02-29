4da4f94832b69605c8ab2f0d360c4d2e3e4b068f
Milos Subotic <milos.subotic.sm@gmail.com>
2020-02-29 12:21:11 +0100

    Have external reset.

--- FPGA/nios2_pio/nios2_pio.vhd
+++ FPGA/nios2_pio/nios2_pio.vhd
@@ -14,6 +14,7 @@ entity nios2_pio is
 		-- On MAX1000.
 		-- System signals.
 		i_clk                      :  in std_logic; -- 12MHz clock.
+		in_rst                     :  in std_logic;
 		
 		
 		-- LEDs.
@@ -29,21 +30,27 @@ end entity;
 
 architecture nios2_pio_arch of nios2_pio is
 	
+	signal rst : std_logic;
+	
 	component nios2_pio_qsys is
 		port (
 			i_clk_clk    : in  std_logic                    := 'X';             -- clk
+			i_sw_export  : in  std_logic_vector(7 downto 0) := (others => 'X'); -- export
 			o_led_export : out std_logic_vector(7 downto 0);                    -- export
-			i_sw_export  : in  std_logic_vector(7 downto 0) := (others => 'X')  -- export
+			i_rst_reset  : in  std_logic                    := 'X'              -- reset
 		);
 	end component nios2_pio_qsys;
 	
 begin
 	
+	rst <= not in_rst;
+	
 	u0 : component nios2_pio_qsys
 	port map (
-		i_clk_clk    => i_clk,    -- i_clk.clk
-		o_led_export => o_led, -- o_led.export
-		i_sw_export  => i_sw   --  i_sw.export
+		i_clk_clk    => i_clk,
+		i_sw_export  => i_sw,
+		o_led_export => o_led,
+		i_rst_reset  => rst
 	);
 
 end architecture;
--- FW/pio_playground_02_ptr/pio_playground.c
+++ FW/pio_playground_02_ptr/pio_playground.c
@@ -4,8 +4,8 @@
 #include "io.h"
 #include "system.h"
 
-volatile uint8_t* sw = SW_PIO_BASE;
-volatile uint8_t* led = LED_PIO_BASE;
+volatile uint8_t* sw = (volatile uint8_t*)SW_PIO_BASE;
+volatile uint8_t* led = (volatile uint8_t*)LED_PIO_BASE;
 
 int main() {
 
